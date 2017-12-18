e759d2ed14d2c3b91ed29f517a9eb7637b36e5d7
Cayprol <Cayprol@users.noreply.github.com>
2017-12-18 13:26:38 +0800

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 ﻿{
 	'name': 'view_quotation_from_product',
 	'description': 'Create a smart button on product linking to historical quotation of the particular product',
-	'depends': ['sales'],
-	'data': [], 
+	'depends': ['base', 'sale', 'product'],
+	'data': ['views/product_description.xml'], 
 }
