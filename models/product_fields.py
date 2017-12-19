7b4a0af5327deb10a9a4a365d5498d1f7d78c03d
Cayprol <kof629@gmail.com>
2017-12-19 10:43:50 +0800

    initial git repo setup

---
+++ models/product_fields.py
@@ -0,0 +1,12 @@
+﻿from odoo import models, fields
+
+
+
+
+class Product_Description(models.Model):
+
+	_name = 'product_description'
+
+	_inherit = 'product.product'
+
+	product_description = fields.Char(string='Description for Product')
