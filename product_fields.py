0ae6660abb8deba24609b739d9c3aabfa6ee6629
Cayprol <kof629@gmail.com>
2017-12-25 08:58:18 +0800

    fixing import

---
+++ product_fields.py
@@ -0,0 +1,14 @@
+﻿from odoo import models, fields, api
+
+
+class SaleOrderInherited(models.Model):
+	_inherit = 'sale.order' 
+
+	custom_field = fields.Char(string='Custom Field')
+
+# class ProductInformation(models.Model):
+# 	_inherit = 'product.template'
+
+# 	customer_item = fields.Boolean(string='Ordered') 
+# 	customer_pid = fields.one2many('res.partner', 'name', string='customer_pid')
+
