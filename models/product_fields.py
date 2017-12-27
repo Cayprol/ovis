7c07bb9303b689d814afdf432259109b470eb288
Cayprol <kof629@gmail.com>
2017-12-27 08:12:37 +0800

    adding pid

--- models/product_fields.py
+++ models/product_fields.py
@@ -16,4 +16,4 @@ class ProductInformation(models.Model):
 class CustomerPID(models.Model):
 	_inherit = 'res.partner'
 
-	pid = fields.Many2one('product.template', 'PID')
+	# pid = fields.Many2one('product.template', 'PID')
