223a72b1089d949511b232f7b020cbb518d2de86
Cayprol <kof629@gmail.com>
2017-12-27 15:31:52 +0800

    adding pid

--- models/product_fields.py
+++ models/product_fields.py
@@ -6,12 +6,16 @@ class SaleOrderInherited(models.Model):
 
 	custom_field = fields.Char(string='Custom Field')
 
-class ProductInformation(models.Model):
+class ProductTemplateInherited(models.Model):
 	_inherit = 'product.template'
 
 	afield = fields.Char(string='Atest') 
 
 	customer_pid = fields.One2many('product.customerinfo', 'name', string='Customer PID')
+
+class ResPartnerInherited(modesl.Model):
+	_inherit = 'res.partner'
+
 
 
 class ProductCustomerInfo(models.Model):
@@ -20,10 +24,8 @@ class ProductCustomerInfo(models.Model):
 
 	_description = 'Information about a product customer'
 
-	name = fields.Many2one('res.partner', 'Customer', domain=[('customer', '=', True)], ondelete='restrict', required=True, help='Customer relates to this product')
+	name = fields.Many2one('res.partner', 'Customer', index=True, domain=[('customer', '=', True)], ondelete='restrict', required=True, help='Customer relates to this product')
 
 	pid = fields.Char(string='Product ID', required=True)
 
 	company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id.id, index=1)
-
-	sequence = fields.Integer(string='Sequence', default=1, help='Assigns the priority to the list of product customer.')
