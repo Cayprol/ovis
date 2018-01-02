70600d8e5cc0552e5d556b41ba86ea61601125f1
Cayprol <kof629@gmail.com>
2018-01-02 17:23:49 +0800

    loading

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,12 @@
-﻿#　-*- coding: utf- -*-
+﻿#　-*- coding: utf-8 -*-
 {
 	'name': "OVIS",
 	'summary': 'Custom module for OVIS',
-	'description': """Custom module for OVIS
-		OVIS module for daily operation:
-			- Additional Product Fields
-			- Additional Shortcut to Views
-	""",
+	'description': """OVIS module for daily operation""",
 	'author': "Cayprol",
 	'website': 'http://www.ovis.com.tw',
 	'category': 'Test',
 	'version': '1.0',
 	'depends': ['base', 'sale', 'product'],
 	'data': ['views/OVIS_product_template.xml'],
-	'installable': True,
 }
