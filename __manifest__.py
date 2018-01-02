5906dee93a705de1273dd9aaf2835d79a82ba82e
Cayprol <kof629@gmail.com>
2018-01-02 08:41:13 +0800

    loading module

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,9 @@
 ﻿{
 	'name': 'OVIS',
 	'description': 'Custom module for OVIS',
+	'author': 'Justin Chen',
+	'website': 'http://www.ovis.com.tw',
+	'version': '0.1',
 	'depends': ['base', 'sale', 'product'],
 	'data': ['views/OVIS_product_template.xml'], 
 }
