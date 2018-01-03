259cb777488142cc3b714c6b0eeef1c9e4f748da
Cayprol <kof629@gmail.com>
2018-01-03 14:54:28 +0800

    fixing manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': "OVIS",
 
@@ -26,7 +26,6 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/OVIS_product_template.xml',
-        'views/OVIS_product_template_purchase_button_inherit.xml'
     ],
 }
 
