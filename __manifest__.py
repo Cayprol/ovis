6929d9b078b0a9a8e0635bd0776232001aa560ca
Cayprol <Cayprol@users.noreply.github.com>
2018-01-03 14:56:53 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/OVIS_product_template.xml',
+        'views/OVIS_product_template.xml','views/OVIS_product_template_purchase_button_inherit.xml',
     ],
 }
 
