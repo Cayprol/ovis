31ec7b5d1ca0066189581fd8d1ca4208dc74221c
Cayprol <kof629@gmail.com>
2018-01-04 13:33:57 +0800

    override without depends

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'product', 'purchase'],
+    'depends': ['base', 'sale', 'product'],
 
     # always loaded
     'data': [
