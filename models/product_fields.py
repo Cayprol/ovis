4a1d7efa5b1927dcfd9404cd38b061f09103b081
Cayprol <kof629@gmail.com>
2018-01-08 17:38:11 +0800

    one2many test

--- models/product_fields.py
+++ models/product_fields.py
@@ -52,46 +52,55 @@ class ProductCustomerInfo(models.Model):
 	_order = 'sequence, min_qty desc, price'
 	
 	name = fields.Many2one(
-        'res.partner', 'Vendor',
-        domain=[('supplier', '=', True)], ondelete='cascade', required=True,
-        help="Vendor of this product")
+		'res.partner', 'Vendor',
+		domain=[('supplier', '=', True)], ondelete='cascade', required=True, 
+		help="Vendor of this product")
+
     product_name = fields.Char(
-        'Vendor Product Name',
-        help="This vendor's product name will be used when printing a request for quotation. Keep empty to use the internal one.")
-    product_code = fields.Char(
-        'Vendor Product Code',
-        help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.")
-    sequence = fields.Integer(
-        'Sequence', default=1, help="Assigns the priority to the list of product vendor.")
-    product_uom = fields.Many2one(
-        'product.uom', 'Vendor Unit of Measure',
-        readonly="1", related='product_tmpl_id.uom_po_id',
-        help="This comes from the product form.")
+    	'Vendor Product Name', 
+    	help="This vendor's product name will be used when printing a request for quotation. Keep empty to use the internal one.")
+
+	product_code = fields.Char(
+		'Vendor Product Code', 
+		help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.")
+
+    sequence = fields.Integer( 
+    	'Sequence', default=1, help="Assigns the priority to the list of product vendor.")
+
+	product_uom = fields.Many2one(
+		'product.uom', 'Vendor Unit of Measure', 
+		readonly="1", related='product_tmpl_id.uom_po_id',
+		help="This comes from the product form.")
+
     min_qty = fields.Float(
-        'Minimal Quantity', default=0.0, required=True,
-        help="The minimal quantity to purchase from this vendor, expressed in the vendor Product Unit of Measure if not any, in the default unit of measure of the product otherwise.")
-    price = fields.Float(
-        'Price', default=0.0, digits=dp.get_precision('Product Price'),
-        required=True, help="The price to purchase a product")
-    company_id = fields.Many2one(
-        'res.company', 'Company',
-        default=lambda self: self.env.user.company_id.id, index=1)
-    currency_id = fields.Many2one(
-        'res.currency', 'Currency',
-        default=lambda self: self.env.user.company_id.currency_id.id,
-        required=True)
-    date_start = fields.Date('Start Date', help="Start date for this vendor price")
-    date_end = fields.Date('End Date', help="End date for this vendor price")
-    product_id = fields.Many2one(
-        'product.product', 'Product Variant',
-        help="If not set, the vendor price will apply to all variants of this products.")
-    product_tmpl_id = fields.Many2one(
-        'product.template', 'Product Template',
-        index=True, ondelete='cascade', oldname='product_id')
-    product_variant_count = fields.Integer('Variant Count', related='product_tmpl_id.product_variant_count')
-    delay = fields.Integer(
-        'Delivery Lead Time', default=1, required=True,
-        help="Lead time in days between the confirmation of the purchase order and the receipt of the products in your warehouse. Used by the scheduler for automatic computation of the purchase order planning.")
+		'Minimal Quantity', default=0.0, required=True, 
+    	help="The minimal quantity to purchase from this vendor, expressed in the vendor Product Unit of Measure if not any, in the default unit of measure of the product otherwise.")
+    
+	price = fields.Float(
+		'Price', default=0.0, digits=dp.get_precision('Product Price'),
+		required=True, help="The price to purchase a product")
+    
+	company_id = fields.Many2one(
+		'res.company', 'Company',
+		default=lambda self: self.env.user.company_id.id, index=1)
+    
+	currency_id = fields.Many2one(
+		'res.currency', 'Currency',
+		default=lambda self: self.env.user.company_id.currency_id.id,
+		required=True)
+
+	date_start = fields.Date('Start Date', help="Start date for this vendor price")
+	date_end = fields.Date('End Date', help="End date for this vendor price")
+	product_id = fields.Many2one(
+		'product.product', 'Product Variant',
+		help="If not set, the vendor price will apply to all variants of this products.")
+	product_tmpl_id = fields.Many2one(
+		'product.template', 'Product Template',
+		index=True, ondelete='cascade', oldname='product_id')
+	product_variant_count = fields.Integer('Variant Count', related='product_tmpl_id.product_variant_count')
+	delay = fields.Integer(
+		'Delivery Lead Time', default=1, required=True,
+		help="Lead time in days between the confirmation of the purchase order and the receipt of the products in your warehouse. Used by the scheduler for automatic computation of the purchase order planning.")
 
 # 	_name = 'product.customerinfo'
 
