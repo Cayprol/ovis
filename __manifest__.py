988913951a2c0cfd23084dad16f4358cdc967638
Cayprol <kof629@gmail.com>
2018-10-26 14:39:42 +0800

    rework setup done

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'description': """Custom Module For OVIS.""",
     'author': 'Cayprol',
     "depends" : ['base', 'mrp', 'product', 'purchase', 'sale', 'web'],
-    'data': [
+    'data': ['views/inherit_product_template_only_form_views.xml',
              ],
     "images": [],
     'license': 'LGPL-3',
--- models/product.py
+++ models/product.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 class InheritProductTemplate(models.Model):
 
 	_inherit = 'product.template'
 
 	drawing = fields.Char(string='Drawaing', help="Engineer Drawing file name.")
+
+	
