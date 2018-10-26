119d803468039242e4784e8e6cfe843cdfee5ba6
Cayprol <kof629@gmail.com>
2018-10-26 12:33:51 +0800

    odoo 12 rework 2
    import vs from . import from v11.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,20 @@
-﻿{
-	'name': 'OVIS',
-	'description': 'Custom module for OVIS',
-	'depends': ['base', 'mrp', 'product', 'purchase', 'sale', 'stock', 'web'],
-	'data': ['views/product_template_only_form_views.xml'], 
+# -*- encoding: utf-8 -*-
+{
+    'name': "OVIS",
+    'version': '12.0.0',
+    'summary': 'Custom Module For OVIS.',
+    'category': 'Other',
+    'description': """Custom Module For OVIS.""",
+    'author': 'Cayprol',
+    "depends" : ['base', 'mrp', 'product', 'purchase', 'sale', 'web'],
+    'data': [
+             ],
+    "images": [],
+    'license': 'LGPL-3',
+    'qweb': [
+            ],  
+    
+    'installable': True,
+    'application'   : True,
+    'auto_install'  : False,
 }
