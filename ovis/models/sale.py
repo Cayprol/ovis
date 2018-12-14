f027362892be9c723aa4b726875103a69816bcee
Cayprol <kof629@gmail.com>
2018-12-14 17:00:53 +0800

    bug fix

--- ovis/models/sale.py
+++ ovis/models/sale.py
@@ -9,8 +9,10 @@ class InheritSaleOrder(models.Model):
 
 	_inherit = 'sale.order'
 
-	ask_approve = fields.Date('Ask Approval Date', readonly=1, index=True, copy=False)
-	date_approve = fields.Date('Approval Date', readonly=1, index=True, copy=False)
+	ask_approve_date = fields.Date('Ask Approval Date', readonly=1, index=True, copy=False)
+	approve_date = fields.Date('Approval Date', readonly=1, index=True, copy=False)
+	approver_id = fields.Many2one('res.users', 'Quotation Approver', readonly=True, copy=False, track_visibility='onchange')
+
 	state = fields.Selection([('draft', 'Quotation'),
 							 ('to approve', 'To Approve'),
 							 ('sent', 'Quotation Sent'),
@@ -19,13 +21,15 @@ class InheritSaleOrder(models.Model):
 							 ('cancel', 'Cancelled'),],
 							 string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', track_sequence=3, default='draft')
 
-
 	@api.multi
 	def action_ask_approve(self, force=False):
-		self.write({'state': 'to approve', 'ask_approve': fields.Date.context_today(self)})
+		self.write({'state': 'to approve', 'ask_approve_date': fields.Date.context_today(self)})
 		return {}
 		
 	@api.multi
 	def action_approve(self, force=False):
-		self.write({'state': 'sent', 'date_approve': fields.Date.context_today(self)})
+		self.write({'state': 'sent', 'approve_date': fields.Date.context_today(self)})
+
+		self.approver_id = self.env.user
+				
 		return {}
