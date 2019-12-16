041f2de9ea7fa64f2d1575c2316c5ff7265cec7c
Cayprol <cayprol@MacBook-Pro.twin.star>
2019-12-17 00:14:32 +0800

    code clean
    - remove is_back_order from sale.order, sale_back_order is now a stand alone module on its own
    - remove old views xml files (not belong to v13)
    - clean up unnecessary <data> tags

--- ovis/models/sale.py
+++ ovis/models/sale.py
@@ -6,9 +6,6 @@ class SaleOrder(models.Model):
 
 	_inherit = 'sale.order'
 
-	# Checker field to determine an order being back order or not
-	is_back_order = fields.Boolean('Back Order Status', compute="_back_order_check", store=True, default=False, help="This boolean field indicates whether this Sale Order has not yet delivered quantity or not.")
-
 	def action_send(self):
 		self.ensure_one()
 		self.write({'state': 'sent'})
@@ -30,15 +27,3 @@ class SaleOrder(models.Model):
 			'context': {'parent_model': self._name,
 						'parent_id': self.id,},
 		}
-
-	# Created for is_back_order field, a checker field meant to implement filter of Sales Back Order.
-	@api.depends('order_line.product_uom_qty', 'order_line.qty_delivered', 'state')
-	def _back_order_check(self):
-		if self.state in ('sale', 'done'):
-			diff = [ True if line.qty_delivered - line.product_uom_qty < 0 else False for line in self.order_line ]
-			if any(diff):
-				self.write({'is_back_order': True})
-			else:
-				self.write({'is_back_order': False})
-		else:
-			self.write({'is_back_order': False})
