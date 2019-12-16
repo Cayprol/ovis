c180988d43cf4b08eb4d2a3c578946ef0df2d955
Cayprol <cayprol@Cayprols-MacBook-Pro.local>
2019-12-16 13:19:30 +0800

    sales department requests
    Add 3 modules
    - invoice with bank info in print menu
    - invoice exclude down payment for custom clearance
    - sales back order calculation (incomplete)

---
+++ sale_back_order/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Back Order',
+    'summary': "View backordered Sales",
+    'version': '13.0.0.0.0',
+    'category': 'Sales/Sales',
+    'author': 'Cayprol',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    'depends': ['account', 'sale_management'],
+    'data': ['views/sale_views.xml',],
+    'demo': [],
+    'test': [],
+}
